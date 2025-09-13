e567408ea7b3a4441b918acf681f61d276274fb7
J0hnathanKim <johnnykim0502@gmail.com>
2025-09-13 23:57:55 +0900

    Initial commit

---
+++ src/main/java/com/example/firstproject/controller/FirstController.java
@@ -0,0 +1,21 @@
+package com.example.firstproject.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+
+@Controller
+
+public class FirstController {
+    @GetMapping("/hi")
+    public String niceToMeetYou(Model model){
+        model.addAttribute("username","Johnathan");
+        return "greetings";
+    }
+
+    @GetMapping("/goodbye")
+    public String seeYouNextTime(Model model){
+        model.addAttribute("username", "Johnathan");
+        return "goodbye"; // goodbye.html
+    }
+}
