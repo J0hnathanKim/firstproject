6aca5d6a90fd469913d16f5c1d4898fcaa25764f
Johnathan Kim <99706992+J0hnathanKim@users.noreply.github.com>
2025-09-16 18:32:58 +0900

    Create ArticleController.java

---
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -0,0 +1,14 @@
+package com.example.firstproject.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+
+public class ArticleController {
+    @PostMapping("/article/create")
+    public String createArticle(ArticleForm form){
+        System.out.println(form.toString());
+        //1. Convert DTO to entitiy
+        //2. Save entitiy to DB as a repository
+        return "";
+    }
+
+}
