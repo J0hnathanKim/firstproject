82de003adaf6672622432ef24465dbfa812b6215
Johnathan Kim <99706992+J0hnathanKim@users.noreply.github.com>
2025-09-19 11:43:56 +0900

    Update ArticleController.java

--- src/main/java/com/example/firstproject/controller/ArticleController.java
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -1,9 +1,17 @@
 package com.example.firstproject.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+
+@Controller
 public class ArticleController {
-    @PostMapping("/article/create")
+    @GetMapping("/articles/new")
+    public String newArticleForm(){
+        return "articles/new";
+    }
+    @PostMapping("/articles/create")
     public String createArticle(ArticleForm form){
         System.out.println(form.toString());
         //1. Convert DTO to entitiy
