6538d76c8d809904fb60c63d267e036a3f497307
Johnathan Kim <99706992+J0hnathanKim@users.noreply.github.com>
2025-09-20 13:56:14 +0900

    Update ArticleController.java

--- src/main/java/com/example/firstproject/controller/ArticleController.java
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -17,9 +17,9 @@ public class ArticleController {
     @PostMapping("/articles/create")
     public String createArticle(ArticleForm form){
         System.out.println(form.toString());
-    /*    //1. Convert DTO to entitiy
+        //1. Convert DTO to entitiy
         Article article = form.toEntity();
-        //2. Save entitiy to DB as a repository */
+        //2. Save entitiy to DB as a repository 
         return "";
     }
 
