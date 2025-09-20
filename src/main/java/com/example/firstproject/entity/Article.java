859feb326cec4ef068e5f08495b50989a77ce2b1
Johnathan Kim <99706992+J0hnathanKim@users.noreply.github.com>
2025-09-20 13:58:59 +0900

    Create Article.java

---
+++ src/main/java/com/example/firstproject/entity/Article.java
@@ -0,0 +1,20 @@
+package com.example.firstproject.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class Article {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column
+    private String title;
+
+    @Column
+    private String content;
+}
