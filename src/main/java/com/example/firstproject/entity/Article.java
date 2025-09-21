d0ef061df88276f97af1c206d491d5d40fdfd74d
Johnathan Kim <99706992+J0hnathanKim@users.noreply.github.com>
2025-09-21 18:33:11 +0900

    Update Article.java

--- src/main/java/com/example/firstproject/entity/Article.java
+++ src/main/java/com/example/firstproject/entity/Article.java
@@ -17,4 +17,24 @@ public class Article {
 
     @Column
     private String content;
+
+    public Article(Long id, String title, String content){
+        this.id = id;
+        this.title = title;
+        this.content = content;
+    }
+
+    public Article(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    @Override
+    public String toString() {
+        return "Article{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", content='" + content + '\'' +
+                '}';
+    }
 }
