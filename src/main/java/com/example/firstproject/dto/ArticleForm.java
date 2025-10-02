795fa10ec41ccf88b0b97fdf6e9a75936c58c7c9
Johnathan Kim <99706992+J0hnathanKim@users.noreply.github.com>
2025-10-02 21:58:57 +0900

    Update ArticleForm.java

--- src/main/java/com/example/firstproject/dto/ArticleForm.java
+++ src/main/java/com/example/firstproject/dto/ArticleForm.java
@@ -1,24 +1,18 @@
 package com.example.firstproject.dto;
 
 import com.example.firstproject.entity.Article;
+import lombok.AllArgsConstructor;
+import lombok.ToString;
 
+@AllArgsConstructor
+@ToString
 public class ArticleForm {
     private String title; // title field
     private String content; // content field
 
-    public ArticleForm(String title, String content) {
-        this.title = title;
-        this.content = content;
-    }
 
     //Method that verifies the data has been completely transferred
-    @Override
-    public String toString() {
-        return "ArticleForm{" +
-                "title='" + title + '\'' +
-                ", content='" + content + '\'' +
-                '}';
-    }
+
 
     public Article toEntity() {
         return new Article(null, title, content);
